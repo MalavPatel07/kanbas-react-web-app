561f86f4f2265ed99a549ae0b74d553106c3c20f
Malav Patel <patel.mala@northeastern.edu>
2023-10-27 22:26:41 -0400

    Assignment-dashboard -1

--- src/Kanbas/Dashboard/index.js
+++ src/Kanbas/Dashboard/index.js
@@ -18,37 +18,46 @@ function Dashboard({
     <div>
       <h1 style={{marginTop:'0px',marginLeft:'20px'}}>DASHBOARD</h1>
       <hr style={{marginTop:'0px',marginLeft:'20px'}}/>
-      <h5>Course</h5>
+      
+      
+      <h5 style={{marginTop:'0px',marginLeft:'20px'}}>Published Courses ({courses.length})</h5>
+      
+      <hr style={{marginTop:'0px',marginLeft:'20px'}}/>
+      <h5 style={{marginTop:'0px',marginLeft:'20px'}}>Course</h5>
       <input
         value={course.name}
         className="form-control w-25"
+        style={{marginTop:'0px',marginLeft:'20px'}}
         onChange={(e) => setCourse({ ...course, name: e.target.value })}
       />
       <input
         value={course.number}
         className="form-control w-25"
+        style={{marginTop:'0px',marginLeft:'20px'}}
         onChange={(e) => setCourse({ ...course, number: e.target.value })}
       />
       <input
         value={course.startDate}
         className="form-control w-25"
+        style={{marginTop:'0px',marginLeft:'20px'}}
         type="date"
         onChange={(e) => setCourse({ ...course, startDate: e.target.value })}
       />
       <input
         value={course.endDate}
         className="form-control w-25"
+        style={{marginTop:'0px',marginLeft:'20px'}}
         type="date"
         onChange={(e) => setCourse({ ...course, endDate: e.target.value })}
       />
-      <button className="btn btn-primary btn-sm" 
+      <br />
+      <button style={{marginTop:'0px',marginLeft:'20px'}} className="btn btn-primary btn-sm" 
       onClick={updateCourse}>Update</button>
 
-      <button className="btn btn-success btn-sm" 
+      <button style={{marginTop:'0px',marginLeft:'10px'}} className="btn btn-success btn-sm" 
       onClick={addNewCourse}>Add</button>
-      <hr />
-      <h5 style={{marginTop:'0px',marginLeft:'20px'}}>Published Courses ({courses.length})</h5>
-      <hr style={{marginTop:'0px',marginLeft:'20px'}}/>
+      <br />
+      <br />
       <div className="row row-cols-1 row-cols-md-3 g-4" style={{marginLeft: '16px'}}>
         {courses.map((course, index) => (
           <div className="col" key={course._id}>
