83037f89781196c223226ce2ed31e9da0f883ff4
Malav Patel <patel.mala@northeastern.edu>
2023-12-04 21:04:34 -0500

    A6 no comments

--- src/project/users/client.js
+++ src/project/users/client.js
@@ -18,7 +18,7 @@ export const signout = async () => {
 
 export const account = async () => {
   const response = await request.post(`${USERS_API}/account`);
-  console.log(response)
+  // console.log(response)
   return response.data;
 };
 
